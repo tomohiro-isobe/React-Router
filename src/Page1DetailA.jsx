1330a13c9537d93f259c45ec902ef1759449ccd4
tomohiro-isobe <36503812+tomohiro-isobe@users.noreply.github.com>
2022-01-19 17:34:33 +0900

    ルーティング追加

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,16 @@
+import { useLocation, useHistory } from "react-router-dom";
+
+export const Page1DetailA = () => {
+  const location = useLocation();
+
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
+
+  return (
+    <div>
+      <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </div>
+  );
+};
